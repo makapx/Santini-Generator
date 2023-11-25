89a707f4932e7ca006849a9c4da58d3459ce4072
makapx <parlavecchiom98@gmail.com>
2023-11-25 18:00:44 +0100

    feat(navbar,footer): change components color and add credits

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  authors = [
+    { name: 'Helias', url: 'https://github.com/Helias' },
+    { name: 'makapx', url: 'https://github.com/makapx' },
+    { name: 'infra-blue', url: 'https://github.com/infra-blue' },
+  ];
 
+  technologies = [
+    { name: 'Angular', url: 'https://angular.io/' },
+    { name: 'TailwindCSS', url: 'https://tailwindcss.com/' },
+    { name: 'DaisyUI', url: 'https://daisyui.com/' },
+  ];
 }
