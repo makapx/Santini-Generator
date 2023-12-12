29c6b94915a459e833b4f008da09ef6a58216587
Giuseppe Bonanno <83579728+GiuseppeBnn@users.noreply.github.com>
2023-12-12 21:40:04 +0100

    working workflow to build and push container image on ghcr.io and some minor fixes (#4)
    * workflow to build and push container image on ghcr.io

--- start.sh
+++ start.sh
@@ -3,12 +3,9 @@
 # Start the correct environment for the chosen env variable
 # If no env variable is set, default to production
 MODE="${1:-production}"
-if [ ${MODE} == "production" ]; then
-    echo "Starting production environment"
+if [[ "$MODE" == "production" || "$MODE" == "dev" ]]; then
+    echo "Starting $MODE environment"
     docker-compose up  ${MODE}-service
-elif [ ${MODE} == "dev" ]; then
-    echo "Starting development environment"
-    docker-compose up development-service
 else
     echo "Invalid environment variable"
     exit 1
